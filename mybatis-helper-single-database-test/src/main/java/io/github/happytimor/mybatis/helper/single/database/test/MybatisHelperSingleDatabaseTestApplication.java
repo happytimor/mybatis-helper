8810b23f7d5c53b41072fcfdfcc66928d3794468
chenpeng <ichenpeng@qq.com>
2020-03-11 10:43:35 +0800

    feat: 时区设置

--- mybatis-helper-single-database-test/src/main/java/io/github/happytimor/mybatis/helper/single/database/test/MybatisHelperSingleDatabaseTestApplication.java
+++ mybatis-helper-single-database-test/src/main/java/io/github/happytimor/mybatis/helper/single/database/test/MybatisHelperSingleDatabaseTestApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 /**
  * @author chenpeng
  */
@@ -13,6 +16,11 @@ public class MybatisHelperSingleDatabaseTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisHelperSingleDatabaseTestApplication.class, args);
+    }
+
+    @PostConstruct
+    void setDefaultTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
     }
 
 }
