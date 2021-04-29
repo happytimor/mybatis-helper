0daa59b741bcc1ca06defa50cca5929eaa7f5fc5
chenpeng <ichenpeng@qq.com>
2021-04-29 10:20:26 +0800

    feat: 多线程测试

--- mybatis-helper-single-database-test/src/test/java/io/github/happytimor/mybatis/helper/single/database/test/OomTests.java
+++ mybatis-helper-single-database-test/src/test/java/io/github/happytimor/mybatis/helper/single/database/test/OomTests.java
@@ -1,5 +1,6 @@
 package io.github.happytimor.mybatis.helper.single.database.test;
 
+import io.github.happytimor.mybatis.helper.core.function.SqlFunction;
 import io.github.happytimor.mybatis.helper.core.wrapper.SelectWrapper;
 import io.github.happytimor.mybatis.helper.single.database.test.domain.User;
 import io.github.happytimor.mybatis.helper.single.database.test.service.GenerateService;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author chenpeng
@@ -25,7 +28,7 @@ public class OomTests {
     private UserService userService;
 
     @Test
-    public void bigQuery() throws InterruptedException {
+    public void bigQuery() {
 //        long[] arr = new long[10000000];
         //-Xms32m -Xmx32m
         generateService.generateBatch(((flag, userList) -> {
@@ -37,6 +40,42 @@ public class OomTests {
                         .eq(User::getMarried, user.getMarried())
                         .eq(User::getLastLoginTime, user.getLastLoginTime())
                 );
+            }
+        }));
+    }
+
+    /**
+     * 多线程并发测试
+     */
+    @Test
+    public void multipleThreadTest() {
+        generateService.generateBatch(((flag, userList) -> {
+            AtomicInteger atomicInteger = new AtomicInteger(0);
+            int loopCount = 1000;
+            for (int i = 0; i < loopCount; i++) {
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(new Random().nextInt(5));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    this.userService.selectMap(new SelectWrapper<User>()
+                            .select(SqlFunction.count(SqlFunction.distinct(User::getId)),
+                                    SqlFunction.min(User::getAge),
+                                    SqlFunction.sum(SqlFunction.distinct(User::getAge)))
+                            .eq(User::getAge, new Random().nextInt(20))
+                            .eq(User::getFlag, flag)
+                    );
+                    atomicInteger.incrementAndGet();
+                }).start();
+            }
+
+            while (atomicInteger.get() != loopCount) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }));
     }
