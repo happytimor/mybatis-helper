2500c9f0b5b1c07fce19437120f99bdfaf83834b
chenpeng <ichenpeng@qq.com>
2021-05-18 11:09:36 +0800

    feat: having支持字符串

--- mybatis-helper-core/src/main/java/io/github/happytimor/mybatis/helper/core/wrapper/HavingWrapper.java
+++ mybatis-helper-core/src/main/java/io/github/happytimor/mybatis/helper/core/wrapper/HavingWrapper.java
@@ -1,24 +1,24 @@
 package io.github.happytimor.mybatis.helper.core.wrapper;
 
-import io.github.happytimor.mybatis.helper.core.common.SqlFunctionName;
-import io.github.happytimor.mybatis.helper.core.function.SqlFunction;
 import io.github.happytimor.mybatis.helper.core.metadata.ColumnFunction;
-import io.github.happytimor.mybatis.helper.core.metadata.ColumnWrapper;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * @author chenpeng
  */
 public class HavingWrapper<T> extends OrderWrapper<T> {
 
-    private List<HavingCondition<T>> havingConditionList = new ArrayList<>();
+    private final List<HavingCondition<T>> havingConditionList = new ArrayList<>();
 
     public HavingWrapper<T> havingLt(ColumnFunction<T, ?> columnFunction, Object value) {
         this.havingConditionList.add(new HavingCondition<T>(columnFunction, "<", value));
+        return this;
+    }
+
+    public HavingWrapper<T> havingLt(String columnName, Object value) {
+        this.havingConditionList.add(new HavingCondition<T>(columnName, "<", value));
         return this;
     }
 
@@ -27,8 +27,18 @@ public class HavingWrapper<T> extends OrderWrapper<T> {
         return this;
     }
 
+    public HavingWrapper<T> havingLe(String columnName, Object value) {
+        this.havingConditionList.add(new HavingCondition<T>(columnName, "<=", value));
+        return this;
+    }
+
     public HavingWrapper<T> havingEq(ColumnFunction<T, ?> columnFunction, Object value) {
         this.havingConditionList.add(new HavingCondition<T>(columnFunction, "=", value));
+        return this;
+    }
+
+    public HavingWrapper<T> havingEq(String columnName, Object value) {
+        this.havingConditionList.add(new HavingCondition<T>(columnName, "=", value));
         return this;
     }
 
@@ -37,8 +47,18 @@ public class HavingWrapper<T> extends OrderWrapper<T> {
         return this;
     }
 
+    public HavingWrapper<T> havingGe(String columnName, Object value) {
+        this.havingConditionList.add(new HavingCondition<T>(columnName, ">=", value));
+        return this;
+    }
+
     public HavingWrapper<T> havingGt(ColumnFunction<T, ?> columnFunction, Object value) {
-        this.havingConditionList.add(new HavingCondition<T>(columnFunction, ">", value));
+        this.havingConditionList.add(new HavingCondition<>(columnFunction, ">", value));
+        return this;
+    }
+
+    public HavingWrapper<T> havingGt(String columnName, Object value) {
+        this.havingConditionList.add(new HavingCondition<>(columnName, ">", value));
         return this;
     }
 
@@ -47,13 +67,25 @@ public class HavingWrapper<T> extends OrderWrapper<T> {
         return this;
     }
 
+    public HavingWrapper<T> havingNe(String columnName, Object value) {
+        this.havingConditionList.add(new HavingCondition<T>(columnName, "<>", value));
+        return this;
+    }
+
     static class HavingCondition<T> {
         private ColumnFunction<T, ?> columnFunction;
+        private String columnName;
         private Object value;
         private String connector;
 
         public HavingCondition(ColumnFunction<T, ?> columnFunction, String connector, Object value) {
             this.columnFunction = columnFunction;
+            this.connector = connector;
+            this.value = value;
+        }
+
+        public HavingCondition(String columnName, String connector, Object value) {
+            this.columnName = columnName;
             this.connector = connector;
             this.value = value;
         }
@@ -81,6 +113,14 @@ public class HavingWrapper<T> extends OrderWrapper<T> {
         public void setConnector(String connector) {
             this.connector = connector;
         }
+
+        public String getColumnName() {
+            return columnName;
+        }
+
+        public void setColumnName(String columnName) {
+            this.columnName = columnName;
+        }
     }
 
     /**
@@ -97,8 +137,13 @@ public class HavingWrapper<T> extends OrderWrapper<T> {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < havingConditionList.size(); i++) {
             HavingCondition<T> havingCondition = havingConditionList.get(i);
-            String columnName = this.parseColumnName(havingCondition.getColumnFunction());
-            stringBuilder.append(columnName).append(" ").append(havingCondition.getConnector()).append(" ").append(havingCondition.getValue());
+            String columnName = havingCondition.getColumnName();
+            if (columnName == null || "".equals(columnName)) {
+                columnName = this.parseColumnName(havingCondition.getColumnFunction());
+            }
+
+            stringBuilder.append(columnName).append(" ").append(havingCondition.getConnector())
+                    .append(" ").append(havingCondition.getValue());
             if (i != havingConditionList.size() - 1) {
                 stringBuilder.append(" AND ");
             }
