5c62ab269751a23afa81eba12b81c0a8c4661d32
chenpeng <ichenpeng@qq.com>
2022-09-23 14:50:01 +0800

    feat: 支持批量插入返回主键

--- mybatis-helper-core/src/main/java/io/github/happytimor/mybatis/helper/core/method/BatchInsert.java
+++ mybatis-helper-core/src/main/java/io/github/happytimor/mybatis/helper/core/method/BatchInsert.java
@@ -1,10 +1,15 @@
 package io.github.happytimor.mybatis.helper.core.method;
 
+import io.github.happytimor.mybatis.helper.core.common.Constants;
+import io.github.happytimor.mybatis.helper.core.common.IdType;
 import io.github.happytimor.mybatis.helper.core.common.Params;
 import io.github.happytimor.mybatis.helper.core.common.SqlMethod;
+import io.github.happytimor.mybatis.helper.core.mapper.MultipleTableMapper;
 import io.github.happytimor.mybatis.helper.core.metadata.Result;
 import io.github.happytimor.mybatis.helper.core.metadata.TableInfo;
 import io.github.happytimor.mybatis.helper.core.util.SqlScriptUtils;
+import org.apache.ibatis.executor.keygen.Jdbc3KeyGenerator;
+import org.apache.ibatis.executor.keygen.KeyGenerator;
 import org.apache.ibatis.executor.keygen.NoKeyGenerator;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlSource;
@@ -21,7 +26,25 @@ public class BatchInsert extends AbstractMethod {
         String valuesScript = SqlScriptUtils.convertForeach(this.generateValueScript(tableInfo), Params.LIST, null, null, null, "item", ",");
         String script = String.format(sqlMethod.getSql(), this.parseTableName(), generateColumnScript(tableInfo), valuesScript);
         SqlSource sqlSource = languageDriver.createSqlSource(configuration, script, modelClass);
-        return this.addInsertMappedStatement(java.util.Map.class, sqlMethod.getMethod(), sqlSource, new NoKeyGenerator(), null, null);
+
+        KeyGenerator keyGenerator = NoKeyGenerator.INSTANCE;
+        String keyProperty = null;
+        String keyColumn = null;
+        // 表包含主键处理逻辑,如果不包含主键当普通字段处理
+        if (tableInfo.getKeyProperty() != null && tableInfo.getKeyProperty().length() > 0) {
+            if (tableInfo.getIdType() != IdType.DYNAMIC_GENERATE) {
+                keyGenerator = Jdbc3KeyGenerator.INSTANCE;
+            }
+            keyProperty = tableInfo.getKeyProperty();
+            boolean splitTable = MultipleTableMapper.class.isAssignableFrom(mapperClass);
+            if (splitTable) {
+                keyProperty = Params.ENTITY + Constants.DOT + keyProperty;
+            }
+
+            keyColumn = tableInfo.getKeyColumn();
+        }
+
+        return this.addInsertMappedStatement(java.util.Map.class, sqlMethod.getMethod(), sqlSource, keyGenerator, keyProperty, keyColumn);
     }
 
     private String generateColumnScript(TableInfo tableInfo) {
